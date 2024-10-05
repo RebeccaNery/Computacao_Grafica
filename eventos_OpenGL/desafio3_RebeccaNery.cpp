b1feb72bd74fd3a19d69a7f4ec746ac4df02045d
Rebecca Nery <112131032+RebeccaNery@users.noreply.github.com>
2024-10-05 00:10:52 -0300

    commit

--- eventos_OpenGL/desafio3_RebeccaNery.cpp
+++ eventos_OpenGL/desafio3_RebeccaNery.cpp
@@ -5,6 +5,7 @@
 int xi, yi, xf, yf, auxiliar, contador=0;
 bool primeiroclique = false;
 bool segundoclique = false;
+int formaGeometrica = 0;
 
 void initializeGlut(int argc, char **argv){
     glutInit(&argc, argv);
@@ -28,37 +29,6 @@ void limparTela() {
     //glFlush();  
 }
 
-
-void desenhaTrianguloProfessor(){
-    glClear(GL_COLOR_BUFFER_BIT);
-    //Desenhar algo na tela
-    glPointSize(4.0);
-    glLineWidth(4.0);
-    glBegin(GL_TRIANGLES);
-        glColor3f(1.0f, 0.0f, 0.0f);    //defini a cor vermelho
-        glVertex2i(250, 100);           //desenho o primeiro vertice
-        glColor3f(0.0f, 1.0f, 0.0f);    //defini a cor verde
-        glVertex2i(100, 350);           //desenho o segundo vertice
-        glColor3f(0.0f, 0.0f, 1.0f);    //defini a cor azul
-        glVertex2i(400, 350);           //desenho o terceiro vertice
-    glEnd();
-    glFlush();
-}
-void teclado(GLubyte tecla, GLint x, GLint y){
-    //printf("tecla %c pressionada com o cursor na posicao x=%d; y=%d\n", tecla, x, y);
-    if (tecla == 'r' || tecla == 'R'){
-        glColor3f(1.0f, 0.0f, 0.0f); //vermelho
-    }else if (tecla == 'g' || tecla == 'G'){
-        glColor3f(0.0f, 1.0f, 0.0f); //verde
-    }else if (tecla == 'b' || tecla == 'B'){
-        glColor3f(0.0f, 0.0f, 1.0f); //azul
-    }
-}
-
-void tecladoEspecial(GLint tecla, GLint x, GLint y){
-    printf("tecla %d pressionada com o cursor na posicao x=%d; y=%d\n", tecla, x, y);
-}
-
 void desenhaPonto(GLint x, GLint y){
     glColor3f(0.0f, 0.0f, 1.0f); //azul
     glPointSize(8.0);
@@ -76,6 +46,56 @@ void desenhaLinha(GLint xi, GLint yi, GLint xf, GLint yf){
     glEnd();
     glFlush();
 }
+
+void desenhaRetanguloCliques(){
+       
+        glBegin(GL_QUADS);
+            glVertex2i(xi, yi);
+            glVertex2i(xf, yi);
+            glVertex2i(xf, yf);
+            glVertex2i(xi, yf);
+        glEnd();
+    
+    
+    glFlush();
+}
+
+void desenhaTrianguloProfessor(){
+    glClear(GL_COLOR_BUFFER_BIT);
+    //Desenhar algo na tela
+    glPointSize(4.0);
+    glLineWidth(4.0);
+    glBegin(GL_TRIANGLES);
+        glColor3f(1.0f, 0.0f, 0.0f);    //defini a cor vermelho
+        glVertex2i(250, 100);           //desenho o primeiro vertice
+        glColor3f(0.0f, 1.0f, 0.0f);    //defini a cor verde
+        glVertex2i(100, 350);           //desenho o segundo vertice
+        glColor3f(0.0f, 0.0f, 1.0f);    //defini a cor azul
+        glVertex2i(400, 350);           //desenho o terceiro vertice
+    glEnd();
+
+if(formaGeometrica == 1){
+    desenhaPonto(xi, yi);
+}
+
+    glFlush();
+}
+void teclado(GLubyte tecla, GLint x, GLint y){
+    //printf("tecla %c pressionada com o cursor na posicao x=%d; y=%d\n", tecla, x, y);
+    if (tecla == 'r' || tecla == 'R'){
+        glColor3f(1.0f, 0.0f, 0.0f); //vermelho
+    }else if (tecla == 'g' || tecla == 'G'){
+        glColor3f(0.0f, 1.0f, 0.0f); //verde
+    }else if (tecla == 'b' || tecla == 'B'){
+        glColor3f(0.0f, 0.0f, 1.0f); //azul
+    }
+}
+
+void tecladoEspecial(GLint tecla, GLint x, GLint y){
+    printf("tecla %d pressionada com o cursor na posicao x=%d; y=%d\n", tecla, x, y);
+}
+
+
 
 void controleDeCliques(GLint botao, GLint acao, GLint x, GLint y) {
     
@@ -96,36 +116,13 @@ void controleDeCliques(GLint botao, GLint acao, GLint x, GLint y) {
             printf("segundo clique ==> xf: %d, yf: %d\n", xf, yf);
             desenhaPonto(xf, yf);
             desenhaLinha(xi, yi, xf, yf);
-            glBegin(GL_QUADS);
-                glVertex2i(xi, yi);
-                glVertex2i(xf, yi);
-                glVertex2i(xf, yf);
-                glVertex2i(xi, yf);
-            glEnd();
+            desenhaRetanguloCliques();
             primeiroclique = false;
         }
     }
 }
 
-/*void desenhaRetanguloCliques(){
-    glClear(GL_COLOR_BUFFER_BIT);
 
-    if (primeiroclique){
-        desenhaPonto(xi, yi);
-    }
-
-    if(segundoclique){ 
-        desenhaPonto(xf, yf);        
-        glBegin(GL_QUADS);
-            glVertex2i(xi, yi);
-            glVertex2i(xf, yi);
-            glVertex2i(xf, yf);
-            glVertex2i(xi, yf);
-        glEnd();
-    }
-    
-    glFlush();
-}*/
 
 void regras_menu(GLint opcao) {
     switch (opcao) {
@@ -146,7 +143,7 @@ void regras_submenu(GLint opcao) {
     switch (opcao) {
         case 1:
             printf("Opção 'Desenhar Ponto' selecionada\n");
-            desenhaPonto(xi, yi);
+            formaGeometrica = 1;
             break;
         case 2:
             printf("Opção 'Desenhar Linha' selecionada\n");
@@ -222,7 +219,6 @@ int main(int argc, char **argv)
 // ----------------------- O GLUT SÓ ACEITA UMA FUNÇÃO DE DISPLAY DE CADA VEZ!!! --------------------
     glutDisplayFunc(desenhaTrianguloProfessor);
     //glutDisplayFunc(desenhaRetanguloCliques);
-    //glutDisplayFunc(desenha);
 
 // ----------------------- FUNÇÕES DO TECLADO  --------------------
     //glutKeyboardFunc(teclado);
@@ -234,7 +230,7 @@ int main(int argc, char **argv)
     //glutMotionFunc(mouseMovimento);               //mouse pressionado e movendo ao mesmo tempo
     //glutPassiveMotionFunc(mouseMovimentoPassivo); //mouse movendo sem pressionar
     
-//criarMenu();
+    criarMenu();
 
 
     glutMainLoop();                                 //sempre no final colocar em loop infinito  --> mantém a janela aberta
